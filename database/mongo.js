7032488c72c91ab2d03f8601512d9f1c96c9174e
unknown <bodhiswattwac@gmail.com>
2021-10-21 17:16:30 +0530

    Database error

--- database/mongo.js
+++ database/mongo.js
@@ -1,11 +1,9 @@
-const mongoose= require("mongoose")
-
-mongoose.connect("mongodb://127.0.0.1:27017/todoapp",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-
-})
- .then(()=>{
-     console.log("Connected to Database")
- })
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://todo_app:bodhi1234@cluster0.sxea1.mongodb.net/todo_app?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+ 
---
+++ utils/requests.js
@@ -0,0 +1,86 @@
+const Todo = require("../models/mongo")
+
+exports.getTodoById=(req,res,next,todoId)=>{
+    Todo.findById(todoId).exec((err,todo)=>{
+        if(err||!todo){
+            console.log(err)
+            return res.status(400).json(
+                {
+                    error:"404 Todo Not found"
+                }
+            )
+        }
+        req.todo = todo;
+
+        next();
+    })
+}
+
+exports.getAllTodos = (req,res)=>{
+    Todo.find()
+    .sort("-createdAt")
+    .exec((err, todos)=>{
+
+        if(err || !todos){
+
+            console.log(err)
+            return res.status(400).json({
+                error:"Something went wrong in finding all todos"
+            })
+        }
+
+        res.json(todos)
+    })
+}
+
+exports.getTodo =(req,res)=>{
+    return res.json(req.todo)
+}
+
+exports.createTodo = (req,res)=>{
+    const todo = new Todo(req.body)
+
+    todo.save((err,task)=>{
+        if(err||!task){
+            console.log(err)
+            return res.status(400).json({
+                error:"Something went wrong while creating"
+            })
+        }
+        res.json({task})
+    })
+}
+
+exports.updateTodo =(req,res)=>{
+      const todo = req.todo
+
+      todo.task = req.body.task
+
+      todo.save((err, t)=>{
+          if(err||!t){
+            return res.status(400).json({
+                error:"Something went wrong while updating"
+            })
+      }
+      res.json(t);
+    })
+}
+
+exports.deleteTodo =(req,res)=>{
+    const todo = req.todo
+
+    todo.remove((err,task)=>{
+        if(err||!task)
+        {
+            return res.status(400).json({
+                error:"Something went wrong while deleting the todo"
+            })
+        }
+
+        res.json({
+            task_deleted:task,
+            message:"Todo was deleted successfully",
+        })
+
+    })
+}
